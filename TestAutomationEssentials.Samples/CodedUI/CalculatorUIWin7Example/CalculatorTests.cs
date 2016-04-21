005950bfa29c2dd3c1a2aa4458f87c9e2453a2b4
Ian Ceicys <Zeo01@hotmail.com>
2016-04-21 09:57:45 -0400

    Adding ApplicationUnderTest.Launch with System Variable
    Adding Application.Launch and using a System Variable

--- TestAutomationEssentials.Samples/CodedUI/CalculatorUIWin7Example/CalculatorTests.cs
+++ TestAutomationEssentials.Samples/CodedUI/CalculatorUIWin7Example/CalculatorTests.cs
@@ -11,7 +11,7 @@ using Keyboard = Microsoft.VisualStudio.TestTools.UITesting.Keyboard;
 using TestAutomationEssentials.CodedUI;
 using Microsoft.VisualStudio.TestTools.UITesting.WpfControls;
 using Microsoft.VisualStudio.TestTools.UITesting.WinControls;
-
+using System.IO;
 
 namespace CalculatorUIWin7Example
 {
@@ -23,32 +23,56 @@ namespace CalculatorUIWin7Example
     {
         public CalculatorTests()
         {
+
         }
+
+        //Use TestInitialize to run code before running each test 
+        [TestInitialize()]
+        public void TestInitialize()
+        {
+            LaunchTheWindowsApp();
+            // To generate code for this test, select "Generate Code for Coded UI Test" from the shortcut menu and select one of the menu items.
+        }
+
 
         [TestMethod]
         public void Calculator_Add_One_Plus_Two_Should_Eqaul_Three()
         {
 
-            Mouse.Click(UIMap.UICalculatorWindow.Find<WinButton>(By.Name("1")));
-            Mouse.Click(UIMap.UICalculatorWindow.Find<WinButton>(By.Name("Add")));
-            Mouse.Click(UIMap.UICalculatorWindow.Find<WinButton>(By.Name("2")));
-            Mouse.Click(UIMap.UICalculatorWindow.Find<WinButton>(By.Name("Equals")));
+            Mouse.Click(calcApp.Find<WinButton>(By.Name("1")));
+            Mouse.Click(calcApp.Find<WinButton>(By.Name("Add")));
+            Mouse.Click(calcApp.Find<WinButton>(By.Name("2")));
+            Mouse.Click(calcApp.Find<WinButton>(By.Name("Equals")));
 
             // Verify that the 'DisplayText' property of '0' label equals '3'
-            Assert.AreEqual(UIMap.UICalculatorWindow.Find<WinText>(By.Name("Result")).DisplayText, "3", "Result is incorrect");
+            Assert.AreEqual(calcApp.Find<WinText>(By.Name("Result")).DisplayText, "3", "Result is incorrect");
             // To generate code for this test, select "Generate Code for Coded UI Test" from the shortcut menu and select one of the menu items.
+        }
+
+        private void LaunchTheWindowsApp()
+        {
+            var pathToExe = Environment.GetEnvironmentVariable(
+"TestAutomation-CodedUI-Sample-CalculatorWin7", EnvironmentVariableTarget.User);
+
+            Assert.IsTrue(
+                File.Exists(pathToExe),
+                "Path to exe value does not point to a file that exists.  " +
+                "The value is '{0}'.  Hint: Are you running on Windows 7" +
+                " , this sample does not work on the Win8/Win10 Windows Store Calculator",
+                pathToExe);
+
+            calcApp = ApplicationUnderTest.Launch(pathToExe);
+
+
+
+
         }
 
         #region Additional test attributes
 
         // You can use the following additional attributes as you write your tests:
 
-        ////Use TestInitialize to run code before running each test 
-        //[TestInitialize()]
-        //public void MyTestInitialize()
-        //{        
-        //    // To generate code for this test, select "Generate Code for Coded UI Test" from the shortcut menu and select one of the menu items.
-        //}
+
 
         ////Use TestCleanup to run code after each test has run
         //[TestCleanup()]
@@ -90,5 +114,7 @@ namespace CalculatorUIWin7Example
         }
 
         private UIMap map;
+        private string pathToExe;
+        private ApplicationUnderTest calcApp;
     }
 }
